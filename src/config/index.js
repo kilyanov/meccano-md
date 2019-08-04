ce505dc6638c2f56becd1ca8a0aa1b7388be226b
efimov a.a <paterm@ya.ru>
2019-08-04 22:14:33 +0500

    Local build webpack

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,10 @@ const config = {
         apiURL: 'https://api.meccano.mel-meccano.ru/v1'
     },
     development: {
-        apiURL: 'https://api.meccano.paterm.tk/v1' // 'https://api.meccano.test/v1'
+        apiURL: 'https://api.meccano.paterm.tk/v1'
+    },
+    none: {
+        apiURL: 'https://api.meccano.test/v1'
     }
 };
 
