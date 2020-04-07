3c6371d1921bc998b56dd45d82332f7243f8e284
efimov a.a <paterm@ya.ru>
2020-04-07 21:38:08 +0500

    Что-то случилось с правами. У меня (dev) стоять те же права админа, но не даёт редактировать статьи, даже если выбрать абсолютно все права в "Пользователях" и права в проекте

--- src/components/Shared/ProjectAccess/ProjectAccess.jsx
+++ src/components/Shared/ProjectAccess/ProjectAccess.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
 import {EventEmitter} from "../../../helpers";
 import {EVENTS} from "../../../constants";
 import {isProjectAccess} from "../../../helpers/Tools";
 
 const ProjectAccess = ({permissions, children, replaceComponent, redirect}) => {
-    const currentProject = useSelector(state => state.currentProject);
-    const userProject = currentProject && currentProject.userProject;
-    const canRender = !permissions || isProjectAccess(permissions, userProject);
+    const canRender = !permissions || isProjectAccess(permissions);
 
     if (!canRender && redirect) {
         EventEmitter.emit(EVENTS.REDIRECT, redirect);
