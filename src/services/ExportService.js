254de17113203d03f3dca0e45046117cb34fdbaf
efimov a.a <paterm@ya.ru>
2020-04-14 23:17:00 +0500

    Имя файла при экспорте

--- src/services/ExportService.js
+++ src/services/ExportService.js
@@ -3,8 +3,17 @@ import ApiList from '../api/apiList';
 
 export const ExportService = {
     articles: (projectId, templateId, articleIds, filename) => {
+        const sp = new URLSearchParams();
+
+        sp.set('project', projectId);
+        sp.set('export', templateId);
+
+        if (filename) {
+            sp.set('filename', filename);
+        }
+
         return API.post(
-            `${ApiList.export}?project=${projectId}&export=${templateId}&filename=${filename}`,
+            `${ApiList.export}?${sp.toString()}`,
             articleIds && {articleIds}
         );
     }
