02bfc6ef56514112d0659f09987399fccf80ba5b
efimov a.a <paterm@ya.ru>
2020-07-16 22:03:09 +0500

    MECCANO-40

--- src/components/Documents/Document/index.js
+++ src/components/Documents/Document/index.js
@@ -1,25 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import DocumentIcon from '../../Shared/SvgIcons/DocumentIcon';
-import {DOCUMENT_STATUS, DOCUMENT_STATUS_VALUE} from '../../../constants';
+import { DOCUMENT_STATUS, DOCUMENT_STATUS_VALUE } from '../../../constants';
 import TrashIcon from '../../Shared/SvgIcons/TrashIcon';
 import DownloadIcon from '../../Shared/SvgIcons/DownloadIcon';
-import {DocumentService} from '../../../services';
-import {saveAs} from 'file-saver';
+import { DocumentService } from '../../../services';
+import { saveAs } from 'file-saver';
 import Loader from '../../Shared/Loader/Loader';
 import './document.scss';
-import {FILE_TYPE_ICON} from '../../../constants';
+import { FILE_TYPE_ICON } from '../../../constants';
 import Access from "../../Shared/Access/Access";
-import {PERMISSION} from "../../../constants";
+import { PERMISSION } from "../../../constants";
 import { Link } from "react-router-dom";
 
 const cls = new Bem('document');
 
-const Document = ({className, document, highlighted, canDelete = true, onDelete = () => {}, onClick = () => {}, linkPrefix}) => {
-    const [inProgress, setProgress] = useState(false);
+const Document = ({
+    className,
+    document,
+    disabled = false,
+    highlighted,
+    canDelete = true,
+    onDelete = () => {},
+    onClick = () => {},
+    linkPrefix
+}) => {
+    const [ inProgress, setProgress ] = useState(false);
     const handleDownload = (doc) => {
         setProgress(true);
         DocumentService.download(doc.transactionId).then(response => {
-            const blob = new Blob([response.data], {type: 'application/octet-stream'});
+            const blob = new Blob([ response.data ], { type: 'application/octet-stream' });
 
             saveAs(blob, decodeURI(response.headers['x-filename']));
             setProgress(false);
@@ -32,13 +41,15 @@ const Document = ({className, document, highlighted, canDelete = true, onDelete 
 
         return <DocumentIcon {...cls('icon')} />;
     };
-    
-    console.log(document)
+    const Wrapper = ({ children, ...props }) => (
+        linkPrefix
+            ? <Link to={`${linkPrefix}/${document.id}`} {...props}>{children}</Link>
+            : <div {...props}>{children}</div>
+    );
 
     return (
-        <Link
-            to={`${linkPrefix}/${document.id}`}
-            {...cls('', {highlighted}, className)}
+        <Wrapper
+            {...cls('', { highlighted, disabled }, className)}
             data-id={document.id}
             onClick={onClick}
         >
@@ -58,7 +69,6 @@ const Document = ({className, document, highlighted, canDelete = true, onDelete 
                     <button
                         {...cls('button')}
                         onClick={() => handleDownload(document)}
-                        title={document.filename}
                     >
                         {inProgress ?
                             <Loader radius={8} strokeWidth={3}/> :
@@ -68,7 +78,7 @@ const Document = ({className, document, highlighted, canDelete = true, onDelete 
                 )}
 
                 {canDelete && (
-                    <Access permissions={[PERMISSION.editDocuments]}>
+                    <Access permissions={[ PERMISSION.editDocuments ]}>
                         <button
                             {...cls('button', 'danger')}
                             onClick={() => onDelete(document)}
@@ -78,7 +88,7 @@ const Document = ({className, document, highlighted, canDelete = true, onDelete 
                     </Access>
                 )}
             </section>
-        </Link>
+        </Wrapper>
     );
 };
 
