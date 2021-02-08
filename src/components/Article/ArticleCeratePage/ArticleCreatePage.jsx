f177effc8eb258bfbc8338afc0d46a2f669eb495
mukhindev <mukhin.dev@gmail.com>
2021-02-08 13:55:01 +0200

    Состояние по-умолчанию для разделов 2 и 3

--- src/components/Article/ArticleCeratePage/ArticleCreatePage.jsx
+++ src/components/Article/ArticleCeratePage/ArticleCreatePage.jsx
@@ -96,7 +96,9 @@ class ArticleCreatePage extends Component {
             timeZone: defaultTimeZone,
             inProgress: true,
             loadedSources: [],
-            loadedCities: []
+            loadedCities: [],
+            sectionsTwo: [],
+            sectionsThree: []
         };
     }
 
@@ -753,7 +755,6 @@ class ArticleCreatePage extends Component {
                 break;
             case 'section_three_id':
                 field.options = sectionsThree.map(({ name, id }) => ({ label: name, value: id }));
-                field.isHidden = !sectionsThree.length || false;
                 break;
             case 'genre_id':
                 field.requestService = ArticleService.genre;
